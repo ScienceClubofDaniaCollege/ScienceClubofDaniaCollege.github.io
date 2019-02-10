2c90f6e6d814bc79948f558a5d1848a0d471f330
Nurul Huda <nurulhuda859g@gmail.com>
2019-02-10 09:44:04 +0600

    Added JS support for old browser

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 // Start of NHA's JavaScript
-// Loader
-const fadeOutEffect = (fadeTime) => {
-    let target = document.querySelector('#loader');
-    let loaded = () => {target.style.display = "none"};
-    
-    let fadeEffect = setInterval(function () {
-        if (!target.style.opacity) {
-            target.style.opacity = 1;
-        }
-        if (target.style.opacity > 0) {
-            target.style.opacity -= 0.1;
-        } else {
-            clearInterval(fadeEffect);
-            loaded();
-        }
-    }, fadeTime);
+// Loader Old JS
+var fadeOutEffect = function fadeOutEffect(fadeTime) {
+  var target = document.querySelector('#loader');
+
+  var loaded = function loaded() {
+    target.style.display = "none";
+  };
+
+  var fadeEffect = setInterval(function () {
+    if (!target.style.opacity) {
+      target.style.opacity = 1;
+    }
+
+    if (target.style.opacity > 0) {
+      target.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeEffect);
+      loaded();
+    }
+  }, fadeTime);
 };
+
 document.querySelector('body').onload = fadeOutEffect(50);
+// Loader New JS
+// const fadeOutEffect = (fadeTime) => {
+//     let target = document.querySelector('#loader');
+//     let loaded = () => {target.style.display = "none"};
+    
+//     let fadeEffect = setInterval(function () {
+//         if (!target.style.opacity) {
+//             target.style.opacity = 1;
+//         }
+//         if (target.style.opacity > 0) {
+//             target.style.opacity -= 0.1;
+//         } else {
+//             clearInterval(fadeEffect);
+//             loaded();
+//         }
+//     }, fadeTime);
+// };
+// document.querySelector('body').onload = fadeOutEffect(50);
 
 // Navbar appear on scroll
 window.onscroll = function() {scrollFunction()};
