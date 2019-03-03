58db62e2302bfc147a036d19f31f51225d2918d7
MDzahin <afsarzahin@gmail.com>
2019-03-03 21:23:11 +0600

    added a button in math.html

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@
 // End of NHA's JavaScript
 
 // start of zahins javascript
-
+function showdes() {
+  var x = document.getElementsByName('des');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 // end of zahins javascript
