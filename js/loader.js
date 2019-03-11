dc16a80d3c4102910624c2602a7a6ef144635704
MDzahin <afsarzahin@gmail.com>
2019-03-11 22:01:02 +0600

    created preloader.html in html folder

--- js/loader.js
+++ js/loader.js
@@ -2,28 +2,32 @@
 // Loader Old JS
 function fadeOutEffect(fadeTime) {
     var target = document.querySelector('#loader');
-  
-    function loaded() {target.style.display = "none";};
-  
+
+    function loaded() {
+        target.style.display = "none";
+    };
+
     var fadeEffect = setInterval(
-  
-    function () {
-      if (!target.style.opacity) {target.style.opacity = 1;}
-  
-      if (target.style.opacity > 0) {
-        target.style.opacity -= 0.2;
-      } else {
-        clearInterval(fadeEffect);
-        loaded();
-      }
-    }, fadeTime);
-  };
-  document.querySelector('body').onload = fadeOutEffect(50);
+
+        function () {
+            if (!target.style.opacity) {
+                target.style.opacity = 1;
+            }
+
+            if (target.style.opacity > 0) {
+                target.style.opacity -= 0.2;
+            } else {
+                clearInterval(fadeEffect);
+                loaded();
+            }
+        }, fadeTime);
+};
+document.querySelector('body').onload = fadeOutEffect(50);
 //   // Loader New JS
 //   const fadeOutEffect = (fadeTime) => {
 //       let target = document.querySelector('#loader');
 //       let loaded = () => {target.style.display = "none"};
-      
+
 //       let fadeEffect = setInterval(function () {
 //           if (!target.style.opacity) {
 //               target.style.opacity = 1;
@@ -38,7 +42,7 @@ function fadeOutEffect(fadeTime) {
 //   };
 //   document.querySelector('body').onload = fadeOutEffect(50);
 //   // End of NHA's JavaScript
-  
-  // start of zahins javascript
 
-  // end of zahins javascript
+// start of zahins javascript
+
+// end of zahins javascript
