b9462491f92ad3db768b1837fa90b90d06e69c83
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-12 10:47:57 +0600

    added loader

--- js/loader.js
+++ js/loader.js
@@ -1,28 +1,28 @@
 // Start of NHA's JavaScript
 // Loader Old JS
-function fadeOutEffect(fadeTime) {
-    var target = document.querySelector('#loader');
+// function fadeOutEffect(fadeTime) {
+//     var target = document.querySelector('#loader');
 
-    function loaded() {
-        target.style.display = "none";
-    };
+//     function loaded() {
+//         target.style.display = "none";
+//     };
 
-    var fadeEffect = setInterval(
+//     var fadeEffect = setInterval(
 
-        function () {
-            if (!target.style.opacity) {
-                target.style.opacity = 1;
-            }
+//         function () {
+//             if (!target.style.opacity) {
+//                 target.style.opacity = 1;
+//             }
 
-            if (target.style.opacity > 0) {
-                target.style.opacity -= 0.2;
-            } else {
-                clearInterval(fadeEffect);
-                loaded();
-            }
-        }, fadeTime);
-};
-document.querySelector('body').onload = fadeOutEffect(50);
+//             if (target.style.opacity > 0) {
+//                 target.style.opacity -= 0.2;
+//             } else {
+//                 clearInterval(fadeEffect);
+//                 loaded();
+//             }
+//         }, fadeTime);
+// };
+// document.querySelector('body').onload = fadeOutEffect(50);
 //   // Loader New JS
 //   const fadeOutEffect = (fadeTime) => {
 //       let target = document.querySelector('#loader');
@@ -41,8 +41,33 @@ document.querySelector('body').onload = fadeOutEffect(50);
 //       }, fadeTime);
 //   };
 //   document.querySelector('body').onload = fadeOutEffect(50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
+
 //   // End of NHA's JavaScript
 
 // start of zahins javascript
 
 // end of zahins javascript
+
