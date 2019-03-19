9cf7af6c294cca866b37002056c0395e65092260
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-19 18:04:09 +0600

    nothing

--- js/form.js
+++ js/form.js
@@ -1,29 +1,42 @@
-function check() {
-    var y = document.getElementById("cpassword");
-    var x = document.getElementById("password");
-    if (x.value == y.value) {
-        y.style.border = '3px solid #42dd42';
-
+// Form validation
+var cPassField = document.getElementById("cpassword");
+var passField = document.getElementById("password");
+var submitButton = document.getElementById("submitButton")
+function validate() {
+    if (passField.value == cPassField.value) {
+        cPassField.style.background = '#28a74575';
+        submitButton.removeAttribute("disabled");
     } else {
-        y.style.border = '3px solid red';
+        cPassField.style.background = '#dc35458c';
+        submitButton.setAttribute("disabled", "");
     }
-
 }
 
-function showpass() {
+// Password visibility
+function toglePassVisibility() {
     var openedEye = document.getElementById("eye");
     var closedEye = document.getElementById("eyex");
     var passField = document.getElementById("password");
     if (passField.type === "password") {
         passField.type = "text";
-        // y.value = "---";
-        // y.color = "blue";
         openedEye.style.display = "block";
         closedEye.style.display = "none";
-
     } else {
         passField.type = "password";
         openedEye.style.display = "none";
         closedEye.style.display = "block";
     }
+}
+// Generating member ID
+
+function generateMemberId(){
+    var memberIdContainer = document.getElementById("memberId");
+    var batch = document.getElementById("batch");
+    var shift = document.getElementById("shift");
+    var section = document.getElementById("section");
+    var roll = document.getElementById("roll");
+    var shiftSymbol = (shift.value).slice(0,1);
+    var tempId = batch.value+"-"+shiftSymbol+"-"+section.value+"-"+roll.value.slice(2,20);
+    var id = tempId.slice(4,20);
+    memberIdContainer.value = id;
 }
