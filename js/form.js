36fd958a80866e67e40d99d9f3545cb9931894a8
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-19 15:50:49 +0600

    fixed eye system in login form and also added validation for phone

--- js/form.js
+++ js/form.js
@@ -11,21 +11,19 @@ function check() {
 }
 
 function showpass() {
-    var a = document.getElementById("eye");
-    var b = document.getElementById("eyex");
-    var x = document.getElementById("password");
-    var y = document.getElementById("eye");
-    if (x.type === "password") {
-        x.type = "text";
-        y.value = "---";
-        y.color = "blue";
-        a.style.display = "none";
-        b.style.display = "block";
+    var openedEye = document.getElementById("eye");
+    var closedEye = document.getElementById("eyex");
+    var passField = document.getElementById("password");
+    if (passField.type === "password") {
+        passField.type = "text";
+        // y.value = "---";
+        // y.color = "blue";
+        openedEye.style.display = "block";
+        closedEye.style.display = "none";
 
     } else {
-        x.type = "password";
-        y.value = "👁️";
-        a.style.display = "block";
-        b.style.display = "none";
+        passField.type = "password";
+        openedEye.style.display = "none";
+        closedEye.style.display = "block";
     }
 }
