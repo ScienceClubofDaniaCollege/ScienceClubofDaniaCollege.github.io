27f4443746a0563acfdd1a25a238c722ded44f6b
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2019-03-23 01:05:05 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,13 +109,13 @@ else {navContainer.innerHTML = navBar;}
 var x = document.getElementById("sct");
 var y = document.getElementById("hidesct");
 function hidesct(){
-    if(x.style.left=="0px"){
-        x.style.left="-75px";
-        y.style.left="-68px";
+    if(x.style.left=="-75px"){
+        x.style.left="0px";
+        y.style.left="0px";
         y.style.opacity=".5";
     }else{
-         x.style.left="0px";
-         y.style.left="0px";
+         x.style.left="-75px";
+         y.style.left="-65px";
          y.style.opacity="1";
     }
 }
