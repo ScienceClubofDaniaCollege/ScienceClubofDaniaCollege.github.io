08ccc18f06ee27d577af8d1c27dec4ce0429a829
MDzahin <afsarzahin@gmail.com>
2019-03-22 12:47:18 +0600

    updated theme

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@
 var cPassField = document.getElementById("cpassword");
 var passField = document.getElementById("password");
 var submitButton = document.getElementById("submitButton")
+
 function validate() {
     if (passField.value == cPassField.value) {
         cPassField.style.background = '#28a74575';
@@ -29,14 +30,14 @@ function toglePassVisibility() {
 }
 // Generating member ID
 
-function generateMemberId(){
+function generateMemberId() {
     var memberIdContainer = document.getElementById("memberId");
     var batch = document.getElementById("batch");
     var shift = document.getElementById("shift");
     var section = document.getElementById("section");
     var roll = document.getElementById("roll");
-    var shiftSymbol = (shift.value).slice(0,1);
-    var tempId = batch.value+"-"+shiftSymbol+"-"+section.value+"-"+roll.value.slice(2,20);
-    var id = tempId.slice(4,20);
+    var shiftSymbol = (shift.value).slice(0, 1);
+    var tempId = batch.value + "-" + shiftSymbol + "-" + section.value + "-" + roll.value.slice(2, 20);
+    var id = tempId.slice(4, 20);
     memberIdContainer.value = id;
 }
