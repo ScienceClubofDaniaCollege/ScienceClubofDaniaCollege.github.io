6e6c4a3fda7f2374285ae68b8c9f5765b311b10d
MDzahin <afsarzahin@gmail.com>
2019-03-27 14:37:22 +0600

    updated footer

--- dev/footer.js
+++ dev/footer.js
@@ -1,41 +1,131 @@
 const footerhtml = `
 <br><br><br>
-    <div class="footer row">
-            <div class="card col-md-4 col-sm-12 fzhn">
-                <div class="card-body">
-                    <h5>Sortcut keys</h5>
-                    <hr>
-                    <a href="html/form.html">Registration</a><br>
-                    <a href="html/login.html">Login</a><br>
-                    <a href="html/about-us.html">About us</a><br>
-                    <a href="html/memberlist.html">Member</a><br>
-                    <a href="html/events.html">Events</a>
-                </div>
-            </div>
-            <div class="card col-md-4 col-sm-12 fzhn">
-                <div class="card-body">
-                    <h5>Developed by</h5>
-                    <hr>
-                    <p><i class="fas fa-portrait"></i><a href="http://nhas.me/">Nurul Huda Apon</a></p>
-                    <p><i class="fas fa-portrait"></i><a href="http://zahin.cf/">Md. Zahin Afsar</a></p>
-                </div>
-            </div>
-            <div class="card col-md-4 col-sm-12 fzhn">
-                <div class="card-body">
-                    <h5>Contact</h5>
-                    <hr>
-                    <p><a href="href="tel:+8801787849609"">Nurul Huda Apon</a></p>
-                    <p><a href="href="tel:+8801948297466"">Md. Zahin Afsar</a></p>
-                </div>
-            </div>
-        <div class="icon">
-            <div class="iconfloat">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-linkedin"></i>
-                <i class="fas fa-at"></i>
-            </div>
+
+<footer class="page-footer font-small" style="background-color: #000000c4;color:#d1d1d1;">
+
+    <div style="background-color: #6351ce;">
+      <div class="container">
+
+        <div class="row py-4 d-flex align-items-center">
+
+          <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0">Get connected with us on social networks!</h6>
+          </div>
+
+          <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f white-text mr-4"> </i>
+            </a>
+
+            <a class="tw-ic">
+              <i class="fab fa-twitter white-text mr-4"> </i>
+            </a>
+
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g white-text mr-4"> </i>
+            </a>
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in white-text mr-4"> </i>
+            </a>
+
+            <a class="ins-ic">
+              <i class="fab fa-instagram white-text"> </i>
+            </a>
+
+          </div>
+
+
         </div>
-    </div>`
+
+
+      </div>
+    </div>
+
+
+    <div class="container text-center text-md-left mt-5">
+
+
+      <div class="row mt-3">
+
+
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+
+          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>acrgxzdfzvsgfcbwster <br> vhcnvtyvjehscrtdsghv <br> dytbumvdnscgbdfetygtb, <br> mrvnecrwfyhtjrvnecbg
+          cevytcgbgthy <br> rtncgbethnegbgte.</p>
+
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+
+          <h6 class="text-uppercase font-weight-bold">Events</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">fgydhfvbd</a>
+          </p>
+          <p>
+            <a href="#!">dfvyndxcnxfvb</a>
+          </p>
+          <p>
+            <a href="#!">dcfvgdchfg</a>
+          </p>
+          <p>
+            <a href="#!">edrvtchsfhgb</a>
+          </p>
+
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+
+          <h6 class="text-uppercase font-weight-bold">About</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">sdfzgcv gz</a>
+          </p>
+          <p>
+            <a href="#!">acgxcsd cvf</a>
+          </p>
+          <p>
+            <a href="#!">aszscfc</a>
+          </p>
+          <p>
+            <a href="#!">sczdxv</a>
+          </p>
+
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fas fa-home mr-3"></i> Dhaka, Donia 1236, Bangladesh</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> dania@gmail.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> +8801787849609</p>
+          <p>
+            <i class="fas fa-print mr-3"></i> +nurulhuda????</p>
+
+        </div>
+
+
+      </div>
+
+
+    </div>
+
+    <div class="footer-copyright text-center py-3">Developed by:
+      <a href="https://nhas.me/">Nurul Huda</a>, <a href="https://zahin.cf/">Md zahin Afsar</a>
+    </div>
+
+
+  </footer>`
 const footContainer = document.getElementById("footer");
 footContainer.innerHTML = footerhtml;
