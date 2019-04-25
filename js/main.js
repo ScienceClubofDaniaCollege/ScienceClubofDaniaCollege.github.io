0645e2f3dd6c5b74d8e4a1e8de790458780e21cc
MDzahin <afsarzahin@gmail.com>
2019-04-25 21:18:45 +0600

    added register navigation option

--- js/main.js
+++ js/main.js
@@ -29,6 +29,9 @@ function genNav(p0, p1) {
         <li class="nav-item">
         <a class="nav-link" href="${p1}memberlist.html" id="member-list">Members List</a>
         </li>
+        <li class="nav-item">
+        <a class="nav-link" href="${p1}register.html" id="register">Register</a>
+        </li>
         </ul>
         </div>
         </nav>
