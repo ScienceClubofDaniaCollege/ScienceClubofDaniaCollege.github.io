900887406e166873632f08978a8bfb3f0a0a2e6d
MDzahin <afsarzahin@gmail.com>
2019-05-27 21:45:46 +0600

    help

--- js/gallery.js
+++ js/gallery.js
@@ -1,40 +1,54 @@
-var y = document.getElementsByClassName('year');
-var m = document.getElementsByClassName('month');
-var x = document.getElementById('navbarDropdown');
-function allphoto() {
-  for (var i=0;i<y.length;i+=1){
-	y[i].style.display = 'block';
-	x.innerHTML = 'All Photos';
-}
-for (var i=0;i<m.length;i+=1){
-    m[i].style.display = 'block';
-}
-};
-function year() {
- for (var i=0;i<y.length;i+=1){
-	y[i].style.display = 'block';
-	x.innerHTML = 'This Year';
-}
-for (var i=0;i<m.length;i+=1){
-    m[i].style.display = 'none';
-}
-};
-function month() {
- for (var i=0;i<y.length;i+=1){
-	y[i].style.display = 'none';
-	x.innerHTML = 'This Month';
-}
-for (var i=0;i<m.length;i+=1){
-    m[i].style.display = 'block';
-}
-};
 var btn = document.getElementById('showbtn');
 var inf = document.getElementsByClassName('info');
 var tlt = document.getElementsByClassName('title');
+var y = document.getElementsByClassName('year');
+var m = document.getElementsByClassName('month');
+var x = document.getElementById('navbarDropdown');
 var im = document.getElementsByClassName('img');
+var tb = document.getElementsByTagName('table');
+
+
+
+function allphoto() {
+  for (var i=0;i<y.length;i+=1){
+	y[i].classList.remove('hide');
+	x.innerHTML = 'All Photos';
+}
+for (var i=0;i<m.length;i+=1){
+    m[i].classList.remove('hide');
+}
+};
+
+
+
+function year() {
+ for (var i=0;i<y.length;i+=1){
+	y[i].classList.remove('hide');
+	x.innerHTML = 'This Year';
+}
+for (var i=0;i<m.length;i+=1){
+    m[i].classList.add('hide');
+}
+};
+
+
+
+function month() {
+ for (var i=0;i<y.length;i+=1){
+	y[i].classList.add('hide');
+	x.innerHTML = 'This Month';
+}
+for (var i=0;i<m.length;i+=1){
+    m[i].classList.remove('hide');
+}
+};
+
+
+
+
 if (window.innerWidth <= 660) {
 	for (var i=0;i<inf.length;i+=1){
-	inf[i].style.display = 'none';
+	inf[i].classList.add('hide');
 	btn.classList.remove('hide');
 }
 for (var i=0;i<tlt.length;i+=1){
@@ -44,19 +58,20 @@ for (var i=0;i<im.length;i+=1){
     im[i].style.width = '100%';
 }
 };
-    var btn = document.getElementById('showbtn');
-    var inf = document.getElementsByClassName('info');
-    var tlt = document.getElementsByClassName('title');
+
+
+
 function showdet() {
     if (btn.innerText == 'Show Details') {
         btn.innerText = 'Hide Details';
         for (var i = 0; i < inf.length; i++) {
-            tlt[i].innerHTML =tlt[i].innerHTML -":"-inf[i].innerHTML;
+        	tlt[i].classList.add('hide');
         };
     }
     else {
         btn.innerText = 'Show Details';
         for (var i = 0; i < inf.length; i++) {
+        	tlt[i].classList.remove('hide');
             tlt[i].innerHTML=tlt[i].innerHTML +":"+ inf[i].innerHTML;
         };
     }
