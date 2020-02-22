b319e12d17d7db033cd8f2e2018ff49738897897
Nurul Huda <55424194+nurulhudaapon@users.noreply.github.com>
2020-02-22 23:54:32 +0600

    Update gallery.js

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -3,9 +3,9 @@ let galleryContainer = document.querySelector("#gallery-container");
 const createGallery = async (cat) => {
   let url;
   if (cat) {
-    url = "https://dsc.nhas.me/gallery?cat=" + cat;
+    url = "https://dscapi.herokuapp.com/gallery?cat=" + cat;
   } else {
-    url = "https://dsc.nhas.me/gallery?cat";
+    url = "https://dscapi.herokuapp.com/gallery?cat";
   }
 
   galleryContainer.innerHTML = `<div id="loader" class="center"></div>`
